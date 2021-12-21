2b9186d2078474136fd61f888b887d1ee8ee9628
masum691 <masumhu691@gmail.com>
2021-12-21 17:52:35 +0600

    added more styles

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import styles from '../styles/About.module.css'
 
 const About = () => {
     return (
         <div className="container">
             <h1>Hello Masum</h1>
+            <p className={styles.details}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae porro ipsa dolore tenetur reiciendis qui, odit ratione ullam beatae nisi placeat atque ducimus, odio mollitia quasi, ad maiores aliquid ea consectetur. Vel, omnis?</p>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='container'>
+    <div className={styles.main}>
       <h1>Hellow Next.js!!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consequatur, voluptatibus quibusdam itaque error sunt sed nobis incidunt facere, corrupti, minima molestias?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias sit dolorem dignissimos vel mollitia accusantium, ipsam consectetur quisquam animi veniam aliquid dolorum? Suscipit velit ipsum placeat iste, veritatis nam qui facilis, quibusdam corrupti earum praesentium libero porro unde architecto doloribus eaque quidem tenetur.</p>
       <input type="text" />
+      <br />
+      <button className={styles.btn}>Submit</button>
     </div>
   )
 }
